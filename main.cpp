beedbfc087f3c2772ee54a0f5c55cc7d8d8b18f2
ilya Biryuk <iiya.ps4.com@gmail.com>
2024-04-20 18:58:08 +0300

    prototype

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+#include <QGridLayout>
+#include <QString>
+#include <QLabel>
+#include <QPushButton>
+#include <QFrame>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    // QApplication b(argc, argv);
+    // while (true) {
+    //     int* q = new int(8);
+    // }
+    MainWindow w;
+    // qDebug() << "zalupa";
+    QWidget* qw = new QWidget();
+    auto grid = QGridLayout(qw);
+    //grid.addWidget(new QLabel(QString("row: %1, collum: %2").arg(i).arg(j)), i, j);
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            auto q = new QLabel(QString("row: %1, collum: %2").arg(i).arg(j));
+            q->setFrameShape(QFrame::Box);
+            q->show();
+            grid.addWidget(q, i, j);
+        }
+    }
+    //auto i = QLabel("Hello!", &w);
+    //w.setMenuWidget(new QLabel("Hello!", &w));
+    //w.setCentralWidget(&grid);
+    w.setCentralWidget(qw);
+    w.show();
+    return a.exec();
+}
