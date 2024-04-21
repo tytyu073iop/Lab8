8de0316ee2a92d1f5a48d7fe068aa00be9ac3a5e
tytyu073iop <iiya.ps4.com@gmail.com>
2024-04-21 17:56:59 +0300

    vertical win

--- playgrid.cpp
+++ playgrid.cpp
@@ -10,29 +10,41 @@ void PlayGrid::GetCurPlay(long long y, long long x, size_t cur)
 {
     qDebug() << "GetCurPlay: " << x << " " << y;
     area[x][y] = cur + 1;
-    bool win = false;
+    auto xb = x;
+    auto yb = y;
+   //bool win = false;
     QVector<QWidget*> winItem;
     {
         x -= 2;
         size_t streak = 0;
         for (int i = x; i < x + 5; ++i) {
-            try {
-                if (area.value(i, QVector<size_t>(area.size()))[y] == cur + 1) {
-                    streak++;
-                    winItem.push_back(itemAtPosition(i, y)->widget());
-                    if (streak >= 3) {
-                        qDebug() << "win!";
-                    }
-                } else {
-                    streak = 0;
-                    winItem.clear();
+            if (area.value(i, QVector<size_t>(area.size()))[y] == cur + 1) {
+                streak++;
+                winItem.push_back(itemAtPosition(i, y)->widget());
+                if (streak >= 3) {
+                    qDebug() << "win!";
+                    return;
                 }
-            } catch (...) {
-                break;
+            } else {
+                streak = 0;
+                winItem.clear();
             }
         }
-    }
-    if (win) {
-        qDebug() << "win of: " << (cur == 1 ? "cross" : "circle");
+        y -= 2;
+        x = xb;
+        winItem.clear();
+        for (int i = y; i < y + 5; ++i) {
+            if (area[x].value(i, 0) == cur + 1) {
+                streak++;
+                winItem.push_back(itemAtPosition(x, i)->widget());
+                if (streak >= 3) {
+                    qDebug() << "win!";
+                    return;
+                }
+            } else {
+                streak = 0;
+                winItem.clear();
+            }
+        }
     }
 }
