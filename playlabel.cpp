df9c893fcc65aa31cc4c90a4559c1de108aa6396
tytyu073iop <iiya.ps4.com@gmail.com>
2024-04-21 12:24:37 +0300

    gameplay was done

--- playlabel.cpp
+++ playlabel.cpp
@@ -7,7 +7,12 @@ PlayLabel::PlayLabel(size_t & c, QPixmap * i, size_t x, size_t y) : QLabel(), cu
 void PlayLabel::mousePressEvent(QMouseEvent *event) {
     if (event->button() == Qt::LeftButton) {
         // handle left mouse button here
-        qDebug() << "pressed: " << x << " " << y;
+        // qDebug() << "pressed: " << x << " " << y;
+        if (!block) {
+            this->setPixmap(imgs[cur]);
+            cur = (cur == 1) ? 0 : 1 ;
+            block = true;
+        }
     } else {
         // pass on other buttons to base class
         QLabel::mousePressEvent(event);
--- playlabel.h
+++ playlabel.h
@@ -9,6 +9,7 @@ class PlayLabel : public QLabel
     size_t& cur;
     QPixmap* imgs;
     const size_t x, y;
+    bool block = false;
 public:
     PlayLabel(size_t&, QPixmap*, size_t x, size_t y);
     void mousePressEvent(QMouseEvent *event);
